4f99689a587bf2609c7d7af73d88ffc3d862f1ce
SolChanP <qkrthfcks1@naver.com>
2021-08-16 14:12:51 +0900

    passport-local, jwt 전략 구현, 로그인 및 토큰 인증

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { AccountService } from '../account/account.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private jwtService: JwtService,
+  ) {}
+
+  /** 로그인 요청 사용자 검증  */
+  async validateUser(email: string, password: string) {
+    const account = await this.accountService.findByEmail(email);
+    if (account) {
+      const checkPassword = await bcrypt.compare(
+        password,
+        account.password.substr(8) /** {bcrypt} 제거 전처리 */,
+      );
+      if (checkPassword) {
+        const { password, ...accountWithoutPassword } = account;
+        return accountWithoutPassword;
+      } else return null;
+    }
+    return null;
+  }
+  /** jwt토큰 발급 */
+  async login(email: string) {
+    const payload = { email: email, role: 'user' };
+
+    return `bearer ${this.jwtService.sign(payload)}`;
+  }
 }
---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email', passwordField: 'password' });
+    /** { username: ?, passwordField: ? } 아래 validate와 매칭되기 위한 설정
+     * 입력받는 id가 email이면 여기서 커스텀이 필요하다. */
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const account = this.authService.validateUser(email, password);
+
+    if (!account) new UnauthorizedException();
+    return account;
+  }
+}
