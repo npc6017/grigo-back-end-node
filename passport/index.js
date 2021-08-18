acb1adbed0e6a325bd0a2bd9ca1ccc23b8526def
SolChanP <qkrthfcks1@naver.com>
2021-08-19 01:34:22 +0900

    필요없는 코드 제거

--- passport/index.js
+++ passport/index.js
@@ -4,18 +4,6 @@ const jwt = require('./jwt');
 const { Account } =require('../models');
 
 module.exports = () => {
-    passport.serializeUser((account, done) => {
-        done(null, account.id);
-    });
-    passport.deserializeUser(async  (id, done) => {
-        try{
-            const user = await Account.findOne({where: { id}});
-            done(null, user);
-        }catch (error){
-            console.error(error);
-            done(error);
-        }
-    })
     local();
     jwt();
 }
