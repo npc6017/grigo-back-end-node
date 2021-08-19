84cdab16f75c4fe45c1a33b6ed5069cfdf856789
SolChanP <qkrthfcks1@naver.com>
2021-08-19 22:14:26 +0900

    port -> env.PORT

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from '../httpException.filter';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // class-validator을 사용하기 위해 설정
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
