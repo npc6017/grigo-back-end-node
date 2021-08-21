2a3d9199bbe7ed63abfd0956e3a8c3350298858e
SolChanP <qkrthfcks1@naver.com>
2021-08-21 20:24:36 +0900

    Post update and delete

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,20 @@
-import { Body, Controller, Get, Param, Post, Query, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  HttpException,
+  Param,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RequestPostDto } from './dto/request.post.dto';
 import { PostService } from './post.service';
 import { ResponsePostDTO } from './dto/response.post.dto';
+import { UpdatePostDto } from './dto/update.post.dto';
 
 @Controller('posts')
 export class PostController {
@@ -11,7 +23,10 @@ export class PostController {
   /** Save Post */
   @UseGuards(JwtAuthGuard)
   @Post('/save')
-  async setMyPost(@Request() req, @Body() post: RequestPostDto): Promise<string> {
+  async setMyPost(
+    @Request() req,
+    @Body() post: RequestPostDto,
+  ): Promise<string> {
     /** 글 등록 */
     await this.postService.createPost(req.user.email, post);
 
@@ -30,7 +45,37 @@ export class PostController {
   /** Get Post */
   @UseGuards(JwtAuthGuard)
   @Get('/:postId')
-  async getPost(@Request() req, @Param('postId') postId): Promise<ResponsePostDTO> {
+  async getPost(
+    @Request() req,
+    @Param('postId') postId,
+  ): Promise<ResponsePostDTO> {
     return await this.postService.getMyPost(req.user.email, postId);
+  }
+
+  /** Update Post */
+  @UseGuards(JwtAuthGuard)
+  @Post('/:postId/update')
+  async updatePost(
+    @Request() request,
+    @Param('postId') postId: number,
+    @Body() updatePost: UpdatePostDto,
+  ): Promise<string> {
+    // 본인의 게시글인지 확인
+    const isCheck = await this.postService.checkIsMine(request.user.email);
+    if (!isCheck) throw new ForbiddenException();
+    // 게시글 업데이트 진행
+    await this.postService.update(postId, updatePost);
+    return 'post update successful';
+  }
+
+  /** Delete Post */
+  @UseGuards(JwtAuthGuard)
+  @Post('/:postId/delete')
+  async deletePost( @Request() request, @Param('postId') postId: string): Promise<string> {
+    // 본인의 게시글인지 확인
+    const isCheck = await this.postService.checkIsMine(request.user.email);
+    // 삭제 진행
+    await this.postService.deletePost(postId);
+    return 'post delete successful';
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -9,6 +9,7 @@ import { AccountTag } from '../entities/AccountTag';
 import { AccountService } from '../account/account.service';
 import { Tag } from '../entities/Tag';
 import { ResponsePostDTO } from './dto/response.post.dto';
+import { UpdatePostDto } from './dto/update.post.dto';
 
 @Injectable()
 export class PostService {
@@ -21,6 +22,17 @@ export class PostService {
     @InjectRepository(AccountTag)
     private accountTagRepository: Repository<AccountTag>,
   ) {}
+  /** StringTag To TagObject **/
+  async getTagObject(tags: string[]): Promise<Tag[]> {
+    const tagObjs: Tag[] = [];
+    await Promise.all( tags.map(async (tag) => {
+        const isTag = await this.tagRepository.findOne({ name: tag });
+        if(isTag) tagObjs.push(isTag);
+      }),
+    );
+    return tagObjs;
+  }
+  /** Save Post */
   async createPost(email: string, post: RequestPostDto): Promise<void> {
     // 계정 받아오기
     const account = await this.accountService.findByEmail(email);
@@ -34,19 +46,14 @@ export class PostService {
 
     /** PostTag에 추가, 게시글과 태그 연결 */
     /// 스트링 배열 태그로 디비에서 태크 데이터 가져오기
-    const tagObjs = await Promise.all(
-      post.tags.map((tag) => this.tagRepository.findOne({ name: tag })),
-    );
-    await Promise.all(
-      tagObjs.map((tag) => {
-        // 이번엔 한방쿼리 X 단일 쿼리 반복
-        this.postTagRepository
-          .save(new PostTag(newPost, tag))
-          .catch((error) => {
-            console.error(error);
-          });
-      }),
-    );
+    const tagObjs = await this.getTagObject(post.tags);
+    // 태그 추가 작업을 동기 처리할 필요 없기 때문에 await X
+    tagObjs.map((tag) => {
+      // 이번엔 한방쿼리 X 단일 쿼리 반복
+      this.postTagRepository.save(new PostTag(newPost, tag)).catch((error) => {
+        console.error(error);
+      });
+    });
 
     /** Tag와 관련된 계정의 알림 모두 설정 */
     /// 각 태그를 갖는 계정 모두 가져오기
@@ -139,5 +146,51 @@ export class PostService {
       timeStamp,
       email == account.email ? true : false,
     );
+  }
+
+  async checkIsMine(email: string): Promise<boolean> {
+    const account = await this.accountService.findByEmail(email);
+    const isCheck = await this.postRepository.findOne({
+      where: { account },
+    });
+    return isCheck ? true : false;
+  }
+
+  async update(postId: number, updatePost: UpdatePostDto): Promise<void> {
+    // Post Update(without Tags)
+    const post = await this.postRepository
+      .createQueryBuilder()
+      .update({ title: updatePost.title, content: updatePost.content, boardType: updatePost.boardType, })
+      .whereInIds(postId)
+      .returning('*')
+      .execute()
+      .then((res) => res.raw[0]);
+    // Post Tags Update
+    if (updatePost.addTags) {
+      const newPostTag: PostTag[] = [];
+      const tagObjs = await this.getTagObject(updatePost.addTags);
+      await Promise.all(
+        tagObjs.map(async (tag) => {
+          const exTag = await this.postTagRepository.findOne({
+            where: { post, tag },
+          });
+          if (!exTag) newPostTag.push( this.postTagRepository.create(new PostTag(post, tag)));
+        }),
+      );
+      await this.postTagRepository.save(newPostTag);
+    }
+    if (updatePost.deletedTags) {
+      const tagObjs = await this.getTagObject(updatePost.deletedTags);
+      tagObjs.map((tag) => {
+        this.postTagRepository.delete({
+          post: post,
+          tag: tag,
+        });
+      });
+    }
+  }
+  /** Delete Post */
+  async deletePost(postId: string): Promise<void> {
+    await this.postRepository.createQueryBuilder().delete().whereInIds(postId).execute();
   }
 }
